5af439a413a9004f0cc3d36d9a78b70c43b9e9e1
highpee1991 <ipadeola.it@gmail.com>
2025-07-04 12:35:30 +0100

    Add ROW_NUMBER() window function to rank properties

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,10 +3,18 @@ SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
 
--- rank property by number of bookings
+-- rank properties using RANK()
 SELECT 
- property_id,
- COUNT(*) AS total_bookings,
- RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
+  property_id,
+  COUNT(*) AS total_bookings,
+  RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
+FROM bookings
+GROUP BY property_id;
+
+-- rank properties using ROW_NUMBER()
+SELECT 
+  property_id,
+  COUNT(*) AS total_bookings,
+  ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS property_row_number
 FROM bookings
 GROUP BY property_id;
